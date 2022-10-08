7d1711c5f58cebf9b10a819c8822bca080f03400
Eduard Faus <44145601+Eduard-Faus@users.noreply.github.com>
2022-10-07 20:53:15 -0400

    type = password

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <div class="box has-text-centered v-centered">
         <h1 class="title">EdPass</h1>
         <input class="input" placeholder="Input" bind:value={inp}>
-        <input class="input" placeholder="Password/Salt" bind:value={sal}> 
+        <input type="password" class="input" placeholder="Password/Salt" bind:value={sal}> 
         <button class="button" on:click={hash}>Hash</button>
         <h1 class="subtitle">{password}</h1>
 </div>
