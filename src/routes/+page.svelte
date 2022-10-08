141d17d230b8c6c2d017b40f5c900a85fe593219
Eduard Faus <44145601+Eduard-Faus@users.noreply.github.com>
2022-10-07 21:12:01 -0400

    hide password

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
         let inp = "";
         let sal = "";
         let password = "";
+        let show_password = true
+$: type = show_password ? 'text' : 'password'
+	
+function updateSal (event) {
+	sal = event.target.value
+}
 
         async function hash() {
                 password = "Ed@1" + await sha256(await salt(inp, sal));
@@ -12,7 +18,10 @@
 <div class="box has-text-centered v-centered">
         <h1 class="title">EdPass</h1>
         <input class="input" placeholder="Input" bind:value={inp}>
-        <input type="password" class="input" placeholder="Password/Salt" bind:value={sal}> 
+        <!-- <input type={hidden} class="input" placeholder="Password/Salt" bind:value={sal}>  -->
+        <input class="input" placeholder="Password/Salt" { type } { sal } on:input={ updateSal } />
+        <button class="button"type="button" on:click="{ () => show_password = !show_password }">{show_password ? 'Hide Password/Salt' : 'Show Password/Salt'}</button>
+        <br>
         <button class="button" on:click={hash}>Hash</button>
         <h1 class="subtitle">{password}</h1>
 </div>
